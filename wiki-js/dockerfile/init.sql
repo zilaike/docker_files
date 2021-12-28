d87a9ce1964ce0bddc108296544d082d82ca13d9
zilaike <zilaike@zilaike@hotmail.com>
2021-12-28 09:16:24 +0800

    admin主配置

--- wiki-js/dockerfile/init.sql
+++ wiki-js/dockerfile/init.sql
@@ -1,3 +1,4 @@
 CREATE EXTENSION zhparser;
 CREATE TEXT SEARCH CONFIGURATION chinese (PARSER = zhparser);
-ALTER TEXT SEARCH CONFIGURATION chinese ADD MAPPING FOR n,v,a,i,e,l,j,f WITH simple;
+/*ALTER TEXT SEARCH CONFIGURATION chinese ADD MAPPING FOR n,v,a,i,e,l WITH simple; */
+ALTER TEXT SEARCH CONFIGURATION chinese ADD MAPPING FOR a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z WITH simple;
